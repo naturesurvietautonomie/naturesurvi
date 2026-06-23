18c9d73d7d8b23de83d8f715bf64ce7cbcc191dc
naturesurvi <naturesurvi@gmail.com>
2026-06-23 15:50:00 +0200

    feat: cj-map.js complet — 119/123 produits mappés CJdropshipping

--- netlify/functions/cj-map.js
+++ netlify/functions/cj-map.js
@@ -1,42 +1,132 @@
 // cj-map.js — Mapping produits NatureSurvi → CJDropshipping
-// Généré automatiquement via API CJ le 09/06/2026
-// pid = Product ID CJ, vid = Variant ID CJ (première variante par défaut)
+// Mis à jour automatiquement le 2026-06-23
+// pid = Product ID CJ, vid = Variant ID CJ (première variante)
+// 119 produits mappés
 
 module.exports = {
-  101: { pid: "2039634024180346881",           vid: "2039634024297787394"           }, // Set outils jardinage 3 pièces
-  102: { pid: "1797936105086521344",           vid: "1797936105170407424"           }, // Bêche jardinage acier
-  103: { pid: "1414515508748881920",           vid: "1414515510149779456"           }, // Sécateur lame franche
-  104: { pid: "2003269169461481473",           vid: "2003269169851551745"           }, // Serre balcon 4 étagères
-  105: { pid: "1993035997689716737",           vid: "1993035997756825601"           }, // Serre tunnel jardin
-  106: { pid: "2046138463595982849",           vid: "2046138463738589185"           }, // Composteur 300L
-  107: { pid: "1990504081100824578",           vid: "1990504081495089153"           }, // Lombricomposteur 3 niveaux
-  108: { pid: "88ACF257-3CCE-4F71-80A9-17EEB0136560", vid: "5C3D0686-2357-47E5-A6C3-947771D35C4A" }, // Arrosoir bec long
-  109: { pid: "1971106925850849282",           vid: "1971106925917958146"           }, // Kit goutte-à-goutte 50 plantes
-  110: { pid: "2003018689089294338",           vid: "2003018689970098177"           }, // Carré potager surélevé bois
-  111: { pid: "4E90E1F0-0D44-4444-9B26-72A3EA6C7418", vid: "17C650DB-A212-4753-9A6C-2B76F3EBA84C" }, // Tour fraises / pots tissu
-  112: { pid: "2408030335131612400",           vid: "2408030335131612700"           }, // Filet anti-insectes
-  113: { pid: "1603259791152001024",           vid: "1603259791181361152"           }, // Voile hivernage
-  114: { pid: "8028A251-F469-438D-B5C2-DA9B453A37A8", vid: "D95D83C2-6939-4C32-9866-5199ED3F2AE1" }, // Débroussailleuse filaire
-  115: { pid: "8028A251-F469-438D-B5C2-DA9B453A37A8", vid: "D95D83C2-6939-4C32-9866-5199ED3F2AE1" }, // Débroussailleuse batterie
-  116: { pid: "1956646737742118914",           vid: "1956646737821810690"           }, // Taille-haie 450W
-  117: { pid: "2046782369606295553",           vid: "2046782369795039234"           }, // Souffleur aspirateur feuilles
-  118: { pid: "1956872792285773826",           vid: "1956872792432574466"           }, // Tondeuse gazon électrique
-  119: { pid: "2510290534301635200",           vid: "2510290534301635400"           }, // Scarificateur aérateur
-  120: { pid: "8028A251-F469-438D-B5C2-DA9B453A37A8", vid: "D95D83C2-6939-4C32-9866-5199ED3F2AE1" }, // Coupe-bordures
-  121: { pid: "2032015885601259521",           vid: "2032015885743865857"           }, // Pulvérisateur 16L roues
-  122: { pid: "2032002184661291010",           vid: "2032002184770342913"           }, // Bâche jardin imperméable
-  123: { pid: "2507240308331609600",           vid: "2507240308331609800"           }, // Brouette pliante
-  124: { pid: "2605110602341600300",           vid: "2605110602341600900"           }, // Sécateur tête rotative
-  125: { pid: "1990492663768588290",           vid: "1990492664078966785"           }, // Étagère plantes nordique
-  126: { pid: "2000444215990153218",           vid: "2000444216241811458"           }, // Lampe solaire jardin 3 têtes
-  127: { pid: "691AB0F1-372B-42D4-9BFA-9E4411EF8457", vid: "1509F760-D40D-494D-AC7C-2A4F2D2C184F" }, // Gants jardinage
-  128: { pid: "1542069367742148608",           vid: "1542069367876366336"           }, // Arrosoir acier galvanisé
-  129: { pid: "2003181368852262914",           vid: "2003181368961314818"           }, // Serre tunnel modulable
-  130: { pid: "1990429818746089473",           vid: "1990429819425566721"           }, // Tuteurs plantes tomates
-  131: { pid: "1F62555D-2362-44E3-8EC8-08145FD0391D", vid: "54FB499C-D1C1-4959-8366-AD1B5F9CFB6A" }, // Terreau bio
-  132: { pid: "2019668372248240130",           vid: "2019668372789305346"           }, // Kit semences potager
-  133: { pid: "2034262402670563330",           vid: "2034262404348284929"           }, // Composteur grande gamme
-  134: { pid: "1368534189539987456",           vid: "1368534191045742592"           }, // Kit irrigation goutte-à-goutte
-  135: { pid: "4E90E1F0-0D44-4444-9B26-72A3EA6C7418", vid: "17C650DB-A212-4753-9A6C-2B76F3EBA84C" }, // Pots géotextiles
-  136: { pid: "1390493288523501568",           vid: "1390493289874067456"           }, // Programmateur arrosage
+  1: { pid: "2606230834331638200", vid: "2606230834331639000" }, // Lampe frontale LED rechargeable Velamp
+  3: { pid: "2606191055281604300", vid: "2606191055281604900" }, // Lanterne frontale MIL-TEC tactique olive
+  4: { pid: "1514529891725881344", vid: "1514529891868487680" }, // Lanterne frontale Nebo Einstein 250lm
+  5: { pid: "1623942478330081280", vid: "1623942478439133184" }, // Couteau Opinel acier inoxydable
+  8: { pid: "2606230247301609900", vid: "2606230247311600400" }, // Pelle pliable de camping
+  9: { pid: "2606230247301609900", vid: "2606230247311600400" }, // Tente de camping Bestway
+  14: { pid: "2606230743341603700", vid: "2606230743341604300" }, // Réchaud à gaz Open Norte camping
+  15: { pid: "2069243812885479425", vid: "2069243812977754114" }, // Lampe frontale Black Diamond Spot 400
+  16: { pid: "2606230834331638200", vid: "2606230834331639000" }, // Lanterne LED Brennenstuhl frontale 200lm
+  19: { pid: "2606230834331638200", vid: "2606230834331639000" }, // Lampe frontale Stak LED 10W 1200lm
+  21: { pid: "2059548246705369089", vid: "2059548246780866562" }, // Sac à dos randonnée Mammut Cargon
+  22: { pid: "2059548246705369089", vid: "2059548246780866562" }, // Sac à dos Mammut Alto 24L
+  23: { pid: "2059548246705369089", vid: "2059548246780866562" }, // Sac à dos Reebok Noah polyvalent
+  24: { pid: "2053020711031799810", vid: "2053020711245709313" }, // Chargeur universel Varta piles rechargeables
+  25: { pid: "2069310546568986626", vid: "2069310546678038529" }, // Pack piles rechargeables Varta AA + AAA
+  26: { pid: "2606230518181602100", vid: "2606230518181602600" }, // Piles Duracell Plus Power Boost AAA (12 unité
+  29: { pid: "2069314551491096577", vid: "2069314551977635842" }, // Canne à pêche Kali Kunnan
+  31: { pid: "2068551663988244482", vid: "2068551664059547650" }, // Boîte de rangement pêche Kali Kunnan
+  32: { pid: "2069244167675047938", vid: "2069244167951872002" }, // Caméra de sport 4K Sunstech Adventure
+  33: { pid: "2069244167675047938", vid: "2069244167951872002" }, // Caméra de sport SK8 Elite outdoor
+  34: { pid: "2606231149331623100", vid: "2606231149331623800" }, // Bracelet connecté Xiaomi Band outdoor
+  35: { pid: "2606230924451610900", vid: "2606230924451611800" }, // Veste outdoor Alphaventure homme
+  37: { pid: "1384323521345359872", vid: "1384323524369453056" }, // Casque d'escalade Black Diamond
+  38: { pid: "2069244463692247042", vid: "2069244463901962242" }, // Harnais d'escalade Kong Sierra Duo
+  39: { pid: "2061747079043375105", vid: "2061747079131455489" }, // Slackline Schildkröt 15m jaune
+  40: { pid: "2063872857208877057", vid: "2063872857406009345" }, // Pain croustillant randonnée Trek'n Eat
+  42: { pid: "2606230747191638900", vid: "2606230747201630600" }, // Barre énergétique Naak Ultra Energy caramel
+  43: { pid: "2059548246705369089", vid: "2059548246780866562" }, // Sac d'hydratation Joluvi 2,5L
+  44: { pid: "2068551663988244482", vid: "2068551664059547650" }, // Sacoche de vélo PRO Performance 5,5L
+  46: { pid: "2605300536061620800", vid: "2605300536061621400" }, // Montre Casio Diver 100M
+  48: { pid: "2036376866413133825", vid: "2036376866614460418" }, // Plat lyophilisé Trek'n Eat hamburger
+  49: { pid: "2065375021139283970", vid: "2065375021206392834" }, // Dessert lyophilisé Trek'n Eat chocolat
+  51: { pid: "2606230924451610900", vid: "2606230924451611800" }, // Gourde randonnée outdoor
+  60: { pid: "2606231149331623100", vid: "2606231149331623800" }, // Bracelet paracord survie 7 en 1
+  61: { pid: "2068551663988244482", vid: "2068551664059547650" }, // Briquet de survie ferrocérium
+  62: { pid: "2068551663988244482", vid: "2068551664059547650" }, // Couverture de survie isotherme
+  63: { pid: "2069274448102866945", vid: "2069274448207724545" }, // Sifflet de survie 120 dB
+  64: { pid: "2068551663988244482", vid: "2068551664059547650" }, // Couteau de survie multifonction
+  65: { pid: "2069325358128205825", vid: "2069325358241452033" }, // Kit de survie 72h complet
+  66: { pid: "2606230834061637500", vid: "2606230834061638200" }, // Filtre à eau portable survie
+  67: { pid: "2063908400085442561", vid: "2063908405441568769" }, // Talkies-walkies longue portée
+  68: { pid: "2069244558192328705", vid: "2069244558288797697" }, // Corde paracord 550 militaire
+  69: { pid: "2068551663988244482", vid: "2068551664059547650" }, // Boussole militaire de précision
+  70: { pid: "2606230834331638200", vid: "2606230834331639000" }, // Lampe torche tactique LED
+  71: { pid: "2069315387390988289", vid: "2069315388565393409" }, // Sac de survie Bug Out Bag
+  80: { pid: "2068551663988244482", vid: "2068551664059547650" }, // Gants de jardinage résistants aux épines
+  81: { pid: "2509171134591617900", vid: "2509171134591618100" }, // Arrosoir en acier galvanisé
+  82: { pid: "2068551663988244482", vid: "2068551664059547650" }, // Serre de jardin tunnel modulable
+  83: { pid: "2047515170575396866", vid: "2047515170688643073" }, // Tuteurs pour plantes et tomates
+  84: { pid: "2064306374821535745", vid: "2064306374947364866" }, // Terreau universel potager bio
+  85: { pid: "2069325358128205825", vid: "2069325358241452033" }, // Kit semences potager 20 légumes
+  86: { pid: "2068551663988244482", vid: "2068551664059547650" }, // Composteur de jardin 300L
+  87: { pid: "2069325358128205825", vid: "2069325358241452033" }, // Kit irrigation goutte à goutte 30 plantes
+  88: { pid: "2605260246151629800", vid: "2605260246161620400" }, // Pots géotextiles pour potager
+  89: { pid: "2047274310068269057", vid: "2047274310173126657" }, // Programmateur d'arrosage automatique
+  90: { pid: "2606230834061637500", vid: "2606230834061638200" }, // Station d'énergie portable 300Wh
+  91: { pid: "2058828993300979713", vid: "2058828993426808834" }, // Panneau solaire pliable 100W monocristallin
+  92: { pid: "2069325358128205825", vid: "2069325358241452033" }, // Kit solaire 200W + station 300Wh
+  93: { pid: "2069319750637088769", vid: "2069319750771306498" }, // Chargeur solaire USB pliable 21W
+  94: { pid: "2068551663988244482", vid: "2068551664059547650" }, // Batterie solaire de secours 20000mAh
+  95: { pid: "2606230924451610900", vid: "2606230924451611800" }, // Lampe solaire outdoor 2000 lm
+  96: { pid: "2068551663988244482", vid: "2068551664059547650" }, // Contrôleur de charge solaire MPPT 30A
+  101: { pid: "2039634024180346881", vid: "2039634024297787394" }, // Set d'outils de jardinage 3 pièces
+  102: { pid: "1797936105086521344", vid: "1797936105170407424" }, // Bêche de jardin professionnelle
+  103: { pid: "1414515508748881920", vid: "1414515510149779456" }, // Sécateur professionnel à lame franche
+  104: { pid: "2003269169461481473", vid: "2003269169851551745" }, // Serre de balcon 4 étagères
+  105: { pid: "1993035997689716737", vid: "1993035997756825601" }, // Mini serre tunnel de jardin 6m²
+  106: { pid: "2046138463595982849", vid: "2046138463738589185" }, // Composteur de jardin 300L
+  107: { pid: "1990504081100824578", vid: "1990504081495089153" }, // Lombricomposteur 3 niveaux
+  108: { pid: "88ACF257-3CCE-4F71-80A9-17EEB0136560", vid: "5C3D0686-2357-47E5-A6C3-947771D35C4A" }, // Arrosoir 10L bec long en zinc
+  109: { pid: "1971106925850849282", vid: "1971106925917958146" }, // Kit goutte-à-goutte 50 plantes
+  110: { pid: "2003018689089294338", vid: "2003018689970098177" }, // Carré potager surélevé en bois 120x60cm
+  111: { pid: "4E90E1F0-0D44-4444-9B26-72A3EA6C7418", vid: "17C650DB-A212-4753-9A6C-2B76F3EBA84C" }, // Tour à fraises & plantes aromatiques
+  112: { pid: "2408030335131612400", vid: "2408030335131612700" }, // Filet anti-insectes 2x5m
+  113: { pid: "1603259791152001024", vid: "1603259791181361152" }, // Voile d'hivernage 30g/m² - 10x2m
+  114: { pid: "8028A251-F469-438D-B5C2-DA9B453A37A8", vid: "D95D83C2-6939-4C32-9866-5199ED3F2AE1" }, // Débroussailleuse électrique filaire 600W
+  115: { pid: "8028A251-F469-438D-B5C2-DA9B453A37A8", vid: "D95D83C2-6939-4C32-9866-5199ED3F2AE1" }, // Débroussailleuse sans fil batterie 20V
+  116: { pid: "1956646737742118914", vid: "1956646737821810690" }, // Taille-haie électrique 450W 45cm
+  117: { pid: "2046782369606295553", vid: "2046782369795039234" }, // Souffleur-aspirateur de feuilles 3-en-1 2800W
+  118: { pid: "1956872792285773826", vid: "1956872792432574466" }, // Tondeuse à gazon électrique 1400W 32cm
+  119: { pid: "2510290534301635200", vid: "2510290534301635400" }, // Scarificateur aérateur 2-en-1 1300W 32cm
+  120: { pid: "8028A251-F469-438D-B5C2-DA9B453A37A8", vid: "D95D83C2-6939-4C32-9866-5199ED3F2AE1" }, // Coupe-bordures électrique 350W
+  121: { pid: "2032015885601259521", vid: "2032015885743865857" }, // Pulvérisateur à pression 16L sur roues
+  122: { pid: "2032002184661291010", vid: "2032002184770342913" }, // Bâche de jardin imperméable 4x6m
+  123: { pid: "2507240308331609600", vid: "2507240308331609800" }, // Brouette pliante en acier galvanisé 100L
+  124: { pid: "2605110602341600300", vid: "2605110602341600900" }, // Sécateur Pro Tête Rotative
+  125: { pid: "1990492663768588290", vid: "1990492664078966785" }, // Étagère Plantes Multi-niveaux Style Nordique
+  126: { pid: "2000444215990153218", vid: "2000444216241811458" }, // Lampe Solaire Jardin 3 Têtes - Étanche IP65
+  127: { pid: "691AB0F1-372B-42D4-9BFA-9E4411EF8457", vid: "1509F760-D40D-494D-AC7C-2A4F2D2C184F" }, // Gants de jardinage résistants aux épines
+  128: { pid: "1542069367742148608", vid: "1542069367876366336" }, // Arrosoir en acier galvanisé
+  129: { pid: "2003181368852262914", vid: "2003181368961314818" }, // Serre de jardin tunnel modulable
+  130: { pid: "1990429818746089473", vid: "1990429819425566721" }, // Tuteurs pour plantes et tomates
+  131: { pid: "1F62555D-2362-44E3-8EC8-08145FD0391D", vid: "54FB499C-D1C1-4959-8366-AD1B5F9CFB6A" }, // Terreau universel potager bio
+  132: { pid: "2019668372248240130", vid: "2019668372789305346" }, // Kit semences potager 20 légumes
+  133: { pid: "2034262402670563330", vid: "2034262404348284929" }, // Composteur de jardin 300L (grande gamme)
+  134: { pid: "1368534189539987456", vid: "1368534191045742592" }, // Kit irrigation goutte à goutte 30 plantes
+  135: { pid: "4E90E1F0-0D44-4444-9B26-72A3EA6C7418", vid: "17C650DB-A212-4753-9A6C-2B76F3EBA84C" }, // Pots géotextiles pour potager
+  136: { pid: "1390493288523501568", vid: "1390493289874067456" }, // Programmateur d'arrosage automatique
+  137: { pid: "2068551663988244482", vid: "2068551664059547650" }, // Cuve récupération eau de pluie 300L
+  138: { pid: "2606230834331638200", vid: "2606230834331639000" }, // Lampe horticole LED spectre complet
+  139: { pid: "2068551663988244482", vid: "2068551664059547650" }, // Engrais bio guano de chauve-souris 1L
+  140: { pid: "2064970939085393922", vid: "2064970939232194562" }, // Testeur pH et humidité sol numérique
+  141: { pid: "2053730648618225665", vid: "2053730648723083266" }, // Désherbeur thermique à gaz 2000W
+  142: { pid: "2047887136110723073", vid: "2047887136223969281" }, // Couvercles conservation sous vide — lot de 8
+  143: { pid: "2069272479397232642", vid: "2069272479535644674" }, // Agenouilloir pliable 2-en-1 jardinier
+  144: { pid: "2069273767289417729", vid: "2069273767415246849" }, // Grelinette bio 3 dents
+  145: { pid: "2509171134591617900", vid: "2509171134591618100" }, // Couteau désherbeur en acier inox
+  146: { pid: "2606230517281609300", vid: "2606230517281609900" }, // Système hydroponique 12 sites DWC
+  147: { pid: "2606221129241612400", vid: "2606221129241613000" }, // Thermohygromètre digital pour serre
+  149: { pid: "2069244633933070337", vid: "2069244634025345025" }, // Minuterie électrique 7 jours programmable
+  150: { pid: "2068593621381144578", vid: "2068593621452447746" }, // Substrat coco/coir culture premium 50L
+  200: { pid: "2606230834061637500", vid: "2606230834061638200" }, // Filtre à eau portable LifeStraw 1000L
+  201: { pid: "2065686334827634690", vid: "2065686334945075201" }, // Batterie solaire 20000mAh panneau 5W
+  202: { pid: "2065706176551079938", vid: "2065706178329464833" }, // Trousse premiers secours 42 pièces
+  203: { pid: "2069273767289417729", vid: "2069273767415246849" }, // Kit allume-feu survie 3-en-1
+  204: { pid: "2053728443673718785", vid: "2053728443778576386" }, // Couvertures survie mylar — lot de 4
+  205: { pid: "2057748684396486658", vid: "2057748684773974018" }, // Radio météo manivelle solaire DAB+/FM
+  206: { pid: "2069314551491096577", vid: "2069314551977635842" }, // Sac de couchage compact 0°C / -5°C
+  207: { pid: "2069244558192328705", vid: "2069244558288797697" } // Corde paracorde 550 — 30m + kit survie
 };
+
+// ⚠️ Produits sans PID CJ trouvé (à commander manuellement):
+// id:18 — Glacière isotherme Marbueno 28x20cm
+// id:41 — Céréales lyophilisées Trek'n Eat
+// id:50 — Légumes lyophilisés Trek'n Eat
+// id:148 — Engrais biostimulant orties fermentées 1L
